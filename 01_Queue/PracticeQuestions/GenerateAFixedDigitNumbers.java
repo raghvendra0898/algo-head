d2a62d622167e21a5dba88ec22125909b3d97e3f
Raghvendra Singh <raghvendrasingh0898@gmail.com>
2024-04-01 00:23:31 +0530

    #

---
+++ 01_Queue/PracticeQuestions/GenerateAFixedDigitNumbers.java
@@ -0,0 +1,28 @@
+//Generating 'n' numbers having digit 5 and 6 in it only.
+
+import java.util.*;
+
+
+
+public class GenerateAFixedDigitNumbers {
+    static void fixed(int n)
+    {
+        Queue<String> q = new LinkedList<String>();
+        q.add("5");
+        q.add("6");
+
+        for(int i=0; i<n; i++)
+        {
+            String curr = q.poll();
+            System.out.print(curr + " ");
+            q.add(curr+"5");
+            q.add(curr+"6");
+        }
+
+    }
+    
+    public static void main(String args[])
+    {
+        fixed(10);
+    }
+}
